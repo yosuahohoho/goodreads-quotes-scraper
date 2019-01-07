39adbfff9f38a110c1d386ca19bba260b9c99fb2
yosuahohoho <yosuahohoho@gmail.com>
2019-01-07 19:59:54 +0700

    Success scrape one page

--- index.js
+++ index.js
@@ -4,3 +4,25 @@ const fs = require('fs')
 
 const url = 'https://www.goodreads.com/author/quotes/4918776.Seneca'
 
+function getHtml(url) {
+
+  return rp(url)
+    .then(html => {
+      return $('.quoteText', html)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+
+}
+
+getHtml(url)
+  .then(data => {
+     let quotes = []
+     for(let i = 0; i < data.length; i++){
+       quotes.push({ id: i+1, text: data[i].children[0].data.trim() })
+     }
+
+     console.log(quotes)
+  })
+  .catch(err => console.error(err))
