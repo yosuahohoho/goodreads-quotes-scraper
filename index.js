7e52bcf566c57c517f10f6f90ca78ad72c1b622d
yosuahohoho <yosuahohoho@gmail.com>
2019-01-09 15:49:12 +0700

    Success save scraped data to data.json

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 const rp = require('request-promise')
-const $ = require('cheerio')
+const cheerio = require('cheerio')
 const fs = require('fs')
 
-const url = 'https://www.goodreads.com/author/quotes/4918776.Seneca'
+async function getQuotes() {
+  let quotes = []
+  const options = {
+    uri:'https://www.goodreads.com/author/quotes/4918776.Seneca',
+    transform: function(body) {
+      return cheerio.load(body)
+    }
+  }
 
-function getHtml(url) {
+  try {
+    const $ = await rp(options)
 
-  return rp(url)
-    .then(html => {
-      return $('.quoteText', html)
-    })
-    .catch(err => {
-      console.error(err)
-    })
+    for(let i = 0; i < 30; i++) {
+      const id = i + 1
+      const author = $('.authorOrTitle')[i].children[0].data.trim()
+      const message = $('.quoteText')[i].children[0].data.trim()
 
+      quotes.push({ id, author, message })
+    }
+  
+  }
+  catch(err) {
+    console.error(err)
+  }
+  
+  return quotes
 }
 
-getHtml(url)
-  .then(data => {
-     let quotes = []
-     for(let i = 0; i < data.length; i++){
-       quotes.push({ id: i+1, text: data[i].children[0].data.trim() })
-     }
-
-     console.log(quotes)
+async function saveToJson(obj) {
+  const data = await JSON.stringify(obj, null, 2)
+  await fs.writeFile('./data.json', data, (err) => {
+      if (err) console.error(err)
   })
-  .catch(err => console.error(err))
+
+  return data
+}
+
+async function main() {
+  const result = await getQuotes()
+  const output = await saveToJson(result)
+  console.log(output)
+}
+
+main()
